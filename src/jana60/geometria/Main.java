7682d96c549035eb0abc574c0e4a0342e68e390e
BiagioCozzolino <biagiocozzolino1996@gmail.com>
2022-06-14 14:42:11 +0200

    Creazione rettangolo e immissione dati utente

--- src/jana60/geometria/Main.java
+++ src/jana60/geometria/Main.java
@@ -1,10 +1,22 @@
 package jana60.geometria;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		Rettangolo rettangoloUtente = new Rettangolo();
 
+		System.out.println("Ciao bevenuto al calcola il tuo Rettangolo!");
+		System.out.println("Inserisci i parametri del tuo rettangolo:");
+		System.out.println("Inserisci la base: ");
+		rettangoloUtente.base = scan.nextInt();
+		System.out.println("Inserisci l'altezza: ");
+		rettangoloUtente.altezza = scan.nextInt();
+
+		scan.close();
 	}
 
 }
